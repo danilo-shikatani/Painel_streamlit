b2eb7e874554fcf05bf0b39089c8fcda3bc7d1a4
danilo-shikatani <danilo_shikatani@hotmail.com>
2025-07-03 11:59:06 -0300

    Update painel.py

--- painel.py
+++ painel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Central de Aplicativos", layout="centered")
-st.title("🚀 Central de Aplicações Streamlit")
+st.title("🚀 Central de Aplicações Tesouraria")
 st.markdown("### Escolha um aplicativo para abrir abaixo:")
 
 # Dicionário com os nomes e URLs dos apps
@@ -15,5 +15,5 @@ apps = {
 for nome, url in apps.items():
     if st.button(nome):
         st.markdown(f"[🔗 Abrir {nome} em nova aba]({url})", unsafe_allow_html=True)
-        components.iframe(url, height=800)
+        components.iframe(url, height=1000)
         st.markdown("---")
